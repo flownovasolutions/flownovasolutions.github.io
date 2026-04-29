a8db70087eb5934ab2bb649c3cb8db4515a98a3c
Shelby Williams <flownovasolutions@outlook.com>
2026-04-29 10:58:33 -0500

    added blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
-
   // Passthrough copy
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
@@ -9,11 +8,15 @@ module.exports = function(eleventyConfig) {
   // Copy global folder from _includes
   eleventyConfig.addPassthroughCopy("_includes/global");
 
+  // Date filter for blog posts
+  eleventyConfig.addFilter("dateFormat", function(date) {
+    const d = new Date(date);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return d.toLocaleDateString('en-US', options);
+  });
+
   // Template formats
   eleventyConfig.setTemplateFormats(["njk", "html"]);
-
-  // Use Nunjucks for HTML
-  eleventyConfig.setLibrary("html", require("nunjucks"));
 
   return {
     dir: {
@@ -24,4 +27,3 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
-
